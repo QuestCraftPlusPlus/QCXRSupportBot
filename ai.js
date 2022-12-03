279d32a255fd870972a35a3b3d0e47f413f6af1e
Cart <81428538+Cartrigger@users.noreply.github.com>
2022-12-03 03:03:21 -0500

    Add files via upload

---
+++ ai.js
@@ -0,0 +1,22 @@
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: "",
+});
+const openai = new OpenAIApi(configuration);
+async function ask(prompt) {
+    const response = await openai.createCompletion({
+        model: "curie-instruct-beta",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+    const answer = response.data.choices[0].text;
+    return answer;
+}
+//Export the "ask" function
+module.exports = {
+    ask,
+};
